0108b5ef0b47d140a7f318b67700c82565d0d78c
xuguibin20 <605820133@qq.com>
2023-03-03 23:01:48 +0800

    2023.3.2

---
+++ music-app/src/locale/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue';
+import VueClipboard from 'vue-clipboard2';
+import VueI18n from 'vue-i18n';
+import store from '@/store';
+
+import en from './lang/en.js';
+import zhCN from './lang/zh-CN.js';
+
+Vue.use(VueClipboard);
+Vue.use(VueI18n);
+
+const i18n = new VueI18n({
+  locale: store.state.settings.lang,
+  messages: {
+    en,
+    'zh-CN': zhCN,
+  },
+  silentTranslationWarn: true,
+});
+
+export default i18n;
